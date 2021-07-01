cd65bc7c5163c4559405dcc5535df80e9a63d965
hdinjos <hdinjos@gmail.com>
2021-07-01 23:23:15 +0700

    add base styling

---
+++ src/components/Header.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="wrapper-top">
+    <div class="wrapper-main">
+      <div class="left">
+        <div class="title">AWESOME APP</div>
+      </div>
+      <div class="right">
+        <div class="profile">
+          Account
+          <div class="wrap-account">
+            <div class="btn-1">
+              Profile
+              <!-- <button>Profile</button> -->
+            </div>
+            <div class="btn-1">
+              Logout
+              <!-- <button>Logout</button> -->
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({});
+</script>
+
+<style src="./styles/header.scss" lang="scss" scoped>
+</style>
---
+++ src/components/Table.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <table class="generic-table">
+      <thead>
+        <tr>
+          <th>No</th>
+          <th>Username</th>
+          <th>Status</th>
+          <th>Action</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>1</td>
+          <td>2</td>
+          <td>3</td>
+          <td><button class="btn-table">Assign to Admin</button></td>
+        </tr>
+        <tr>
+          <td>1</td>
+          <td>2</td>
+          <td>3</td>
+          <td><button class="btn-table">Assign to Admin</button></td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<style src="./styles/table.scss" lang="scss" scoped>
+</style>
