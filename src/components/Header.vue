a3c0f6ef1ddf1e3c2f81a7cfe27c540a05b627e0
hdinjos <hdinjos@gmail.com>
2021-07-01 23:34:07 +0700

    change header component

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -23,10 +23,8 @@
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from "vue";
-
-export default defineComponent({});
+<script>
+export default {};
 </script>
 
 <style src="./styles/header.scss" lang="scss" scoped>
